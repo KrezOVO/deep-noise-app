4ba0aff2186e19fe62e075545bd6823b44960782
krez <1307225752@qq.com>
2024-12-09 16:13:12 +0800

    download data

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_file
 import plotly
 import json
 from deepnoiseapp import DeepNoiseApp
 import numpy as np
+import pandas as pd
+import os
+from io import BytesIO
 
 app = Flask(__name__)
 app.debug = True
@@ -33,6 +36,13 @@ def index():
         pred, pred_fft = deep_noise_app.predict([Qv, DP, RPM])
         y = np.array(range(0,5000,64))
         s_x, s_y = stairs(y, pred_fft)
+        
+        # 保存数据用于下载
+        app.pred_data = {
+            'frequency': s_x,
+            'amplitude': s_y
+        }
+        
         graph = dict(
                 data=[
                     dict(
@@ -53,6 +63,30 @@ def index():
         return jsonify(res)
     else:
         return render_template('layout/index.html')
+
+@app.route('/download_data')
+def download_data():
+    try:
+        # 创建DataFrame
+        df = pd.DataFrame(app.pred_data)
+        
+        # 创建BytesIO对象
+        excel_buffer = BytesIO()
+        
+        # 将DataFrame保存到Excel
+        with pd.ExcelWriter(excel_buffer, engine='xlsxwriter') as writer:
+            df.to_excel(writer, sheet_name='预测数据', index=False)
+        
+        excel_buffer.seek(0)
+        
+        return send_file(
+            excel_buffer,
+            mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            as_attachment=True,
+            download_name='prediction_data.xlsx'
+        )
+    except Exception as e:
+        return str(e), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9999)
